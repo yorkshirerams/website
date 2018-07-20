b301d0e0e93034b560a5f7ed57a2399af00bc75d
Mark Gaze <mark.l.gaze@gmail.com>
2018-07-20 13:19:33 +0100

    Fix issue reported by codacy

--- src/components/game/game.jsx
+++ src/components/game/game.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import './game.scss';
 
-const Game = () => {
-  return (
-    <div className="container">
-      <div className="score">
-        <h2>Leeds Bobcats</h2>
-        <h2>0</h2>
-      </div>
-      <div className="score">
-        <h2>Yorkshire Rams</h2>
-        <h2>67</h2>
-      </div>
-      <p>NFC 1 North - 15th July 2018</p>
+const Game = () => (
+  <div className="container">
+    <div className="score">
+      <h2>Leeds Bobcats</h2>
+      <h2>0</h2>
     </div>
-  );
-};
+    <div className="score">
+      <h2>Yorkshire Rams</h2>
+      <h2>67</h2>
+    </div>
+    <p>NFC 1 North - 15th July 2018</p>
+  </div>
+);
 
 export default Game;
