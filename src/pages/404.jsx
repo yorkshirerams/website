527f1ea6943e8f0f809b9ea212c177bfad827dbd
Mark Gaze <mark.l.gaze@gmail.com>
2018-07-20 09:03:46 +0100

    Themed the 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './404.scss';
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div className="not-found">
+    <img className="logo logo404" src="/img/logo.png" alt="Home" />
+    <h1>404 NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... incomplete!</p>
   </div>
 );
 
